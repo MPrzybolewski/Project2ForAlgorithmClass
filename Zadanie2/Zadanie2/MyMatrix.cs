40671ba6d5ef4ea519cbd9b10cef8ecc8ebc6114
Marek Przybolewski <mprzybolewski@gmail.com>
2017-11-04 15:42:37 +0100

    add complementMatrix method and printMatrix method and some simple test of these functions in Program.cs

--- Zadanie2/Zadanie2/MyMatrix.cs
+++ Zadanie2/Zadanie2/MyMatrix.cs
@@ -3,8 +3,44 @@ namespace Zadanie2
 {
     public class MyMatrix<T>
     {
-        public MyMatrix()
+        private int rows;
+        private int columns;
+        private T[,] matrix;
+
+        public MyMatrix(int rows, int columns)
         {
+            this.rows = rows;
+            this.columns = columns;
+            this.matrix = new T[rows,columns];
+        }
+
+        public void complementMatrix(T[,] table)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    matrix[i, j] = table[i, j];
+                }
+            }
+        }
+
+        public void printMatrix()
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                Console.Write("|");
+                for (int j = 0; j < columns; j++)
+                {
+                    if (j != 0)
+                    {
+                        Console.Write(", ");
+                    }                    
+                    Console.Write(matrix[i,j]);
+
+                }
+                Console.WriteLine("|");
+            }
         }
     }
 }
--- Zadanie2/Zadanie2/Program.cs
+++ Zadanie2/Zadanie2/Program.cs
@@ -6,9 +6,8 @@ namespace Zadanie2
     {
         public static void Main(string[] args)
         {
-            MyMatrix<double> doubleMatrix;
-            MyMatrix<float> floatMatrix;
 
+            //Test fraction operations
             MyFraction firstFraction = new MyFraction(2, 5);
             MyFraction secondFraction = new MyFraction(3, 4);
             MyFraction resultMultiply = firstFraction * secondFraction;
@@ -16,6 +15,19 @@ namespace Zadanie2
             Console.WriteLine("Multiply result: Nominator: {0}, Denominator: {1}", resultMultiply.numerator, resultMultiply.denominator);
             Console.WriteLine("Add result: Nominator: {0}, Denominator {1}", resutlAdd.numerator, resutlAdd.denominator);
 
+            MyMatrix<double> doubleMatrix = new MyMatrix<double>(3,4);
+            MyMatrix<float> floatMatrix;
+            MyMatrix<MyFraction> fractionMatrix;
+
+            double[,] tableSample = { { 1, 2, 3, 0 }, { 4, 5, 6, 0 }, { 7, 8, 9, 0 } };
+
+            doubleMatrix.complementMatrix(tableSample);
+            doubleMatrix.printMatrix();
+
+
         }
+
+
+     
     }
 }
