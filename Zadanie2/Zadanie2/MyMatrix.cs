8a85acf6e6770ff68d05c583a6602f6040f19746
Patryk Matuszak <matuszak.patryk@wp.pl>
2017-11-05 21:47:47 +0100

    overload MyMatrix "+" operator

--- Zadanie2/Zadanie2/MyMatrix.cs
+++ Zadanie2/Zadanie2/MyMatrix.cs
@@ -14,6 +14,56 @@ namespace Zadanie2
             this.matrix = new T[rows,columns];
         }
 
+        public static MyMatrix<T> operator +(MyMatrix <T> firstMatrix, MyMatrix <T> secondMatrix)
+        {
+            Type t = firstMatrix.matrix[0, 0].GetType();
+            object result = 0, x  = 0, y = 0;
+            if ((firstMatrix.rows==secondMatrix.rows) && (firstMatrix.columns==secondMatrix.columns))
+            {
+                MyMatrix<T> temp = new MyMatrix<T>(firstMatrix.rows, firstMatrix.columns);
+                for (int i = 0; i < firstMatrix.columns; i++)
+                {
+                    for (int j = 0; j < firstMatrix.rows; j++)
+                    {
+                        if (t == typeof(double))
+                        {
+                            x = Convert.ToDouble(firstMatrix.matrix[j, i].ToString());
+                            y = Convert.ToDouble(secondMatrix.matrix[j, i].ToString());
+                            result = (double)x + (double)y;
+                            temp.matrix[j, i] = (T)result;
+                        }
+
+                        if (t == typeof(float))
+                        {
+                            x = float.Parse(firstMatrix.matrix[j, i].ToString());
+                            y = float.Parse(secondMatrix.matrix[j, i].ToString());
+                            result = (float)x + (float)y;
+                            temp.matrix[j, i] = (T)result;
+                        }
+
+                        if (t == typeof(Int64))
+                        {
+                            x = Convert.ToInt64(firstMatrix.matrix[j, i].ToString());
+                            y = Convert.ToDouble(secondMatrix.matrix[j, i].ToString());
+                            result = (int)x + (int)y;
+                            temp.matrix[j, i] = (T)result;
+                        }
+
+                        if (t == typeof(MyFraction))
+                        {
+                            x = firstMatrix.matrix[j, i];
+                            y = secondMatrix.matrix[j, i];
+                            result = (MyFraction)x + (MyFraction)y;
+                            temp.matrix[j, i] = (T)result;
+                        }
+
+                    }
+                }
+                return temp;
+            }
+            return new MyMatrix<T>(1,1);
+        }
+
         public void complementMatrix(T[,] table)
         {
             for (int i = 0; i < rows; i++)
--- Zadanie2/Zadanie2/Program.cs
+++ Zadanie2/Zadanie2/Program.cs
@@ -16,16 +16,23 @@ namespace Zadanie2
             Console.WriteLine("Add result: Nominator: {0}, Denominator {1}", resutlAdd.numerator, resutlAdd.denominator);
 
             MyMatrix<double> doubleMatrix = new MyMatrix<double>(3,4);
+            MyMatrix<double> doubleMatrix2 = new MyMatrix<double>(3, 4);
             MyMatrix<float> floatMatrix;
             MyMatrix<MyFraction> fractionMatrix = new MyMatrix<MyFraction>(3, 4);
+            MyMatrix<MyFraction> fractionMatrix2 = new MyMatrix<MyFraction>(3, 4);
 
             MyFraction[,] tableSample = new MyFraction[3,4];
+            MyFraction[,] tableSample2 = new MyFraction[3, 4];
+            double[,] doubleTable = new double[3, 4];
+            double[,] doubleTable2 = new double[3, 4];
             int count = 1;
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 4; j++)
                 {
                     tableSample[i,j] = new MyFraction(count);
+                    doubleTable[i, j] = count+0.3;
+                    doubleTable2[i, j] = count+0.3;
                     count++;
 
                 }
@@ -33,8 +40,26 @@ namespace Zadanie2
 
             tableSample[2, 3] = new MyFraction(2,4);
             fractionMatrix.complementMatrix(tableSample);
+            fractionMatrix2.complementMatrix(tableSample);
+            Console.WriteLine("---Przerwa---");
+            doubleMatrix.complementMatrix(doubleTable);
+            doubleMatrix2.complementMatrix(doubleTable2);
+            Console.WriteLine("First double Matrix:");
+            doubleMatrix.printMatrix();
+            Console.WriteLine("Second double Matrix:");
+            doubleMatrix.printMatrix();
+            MyMatrix<double> result = doubleMatrix + doubleMatrix2;
+            Console.WriteLine("Result double Matrix:");
+            result.printMatrix();
+            Console.WriteLine("---Przerwa---");
+            Console.WriteLine("First <T> Matrix:");
             fractionMatrix.printMatrix();
-
+            Console.WriteLine("Second <T> Matrix:");
+            fractionMatrix2.printMatrix();
+            MyMatrix<MyFraction> result2 = fractionMatrix + fractionMatrix2;
+            Console.WriteLine("Result <T> Matrix:");
+            result2.printMatrix();
+            Console.ReadLine();
 
         }
 
