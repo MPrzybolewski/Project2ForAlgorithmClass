4a9a3cc459f001fb840a901301621cb8b6d9e8d1
Marek Przybolewski <mprzybolewski@gmail.com>
2017-11-09 22:38:04 +0100

    add - and / operators to myFraction

--- Zadanie2/Zadanie2/Program.cs
+++ Zadanie2/Zadanie2/Program.cs
@@ -6,13 +6,22 @@ namespace Zadanie2
     {
         public static void Main(string[] args)
         {
-            
-            MyMatrix<double> doubleMatrix = new MyMatrix<double>(4,4);
 
-            double[,] matrixSample = { { 4, -2, 4, -2 }, { 3, 1, 4, 2 }, { 2, 4, 2, 1 }, { 2, -2, 4, 2 } };
+            MyFraction fraction1 = new MyFraction(3, 5);
+            MyFraction fraction2 = new MyFraction(3, 15);
+            MyFraction resultOfSubtraction = fraction1 - fraction2;
+            MyFraction resultOfDivide = fraction1 / fraction2;
+            Console.WriteLine("Result of subtraction:  Numerator: {0}, Denominator: {1}",resultOfSubtraction.numerator,resultOfSubtraction.denominator);
+            Console.WriteLine("Result of divide: Numerator: {0}, Denominator: {1}",resultOfDivide.numerator, resultOfDivide.denominator);
+            MyMatrix<MyFraction> doubleMatrix = new MyMatrix<MyFraction>(4,4);
+
+            MyFraction[,] matrixSample = { { new MyFraction(4), new MyFraction(-2), new MyFraction(4), new MyFraction(-2) },
+                {new MyFraction(3), new MyFraction(1), new MyFraction(4), new MyFraction(2) },
+                {new MyFraction(2), new MyFraction(4), new MyFraction(2), new MyFraction(1) },
+                {new MyFraction(2), new MyFraction(-2), new MyFraction(4), new MyFraction(2) } };
 
             doubleMatrix.complementMatrix(matrixSample);
-            double[] bVector =  { 8, 7, 10, 2 };
+            MyFraction[] bVector =  { new MyFraction(8), new MyFraction(7), new MyFraction(10), new MyFraction(2) };
             bVector = doubleMatrix.gaussWithoutChoice(bVector);
 
             for (int i = 0; i < bVector.Length; i++)
--- Zadanie2/Zadanie2/myFraction.cs
+++ Zadanie2/Zadanie2/myFraction.cs
@@ -3,7 +3,7 @@ using System.Numerics;
 
 namespace Zadanie2
 {
-    public class MyFraction
+    public struct MyFraction
     {
         public BigInteger numerator { get; }
         public BigInteger denominator { get; }
@@ -27,6 +27,19 @@ namespace Zadanie2
             BigInteger numerator = (firstFraction.numerator * secondFraction.denominator) + (secondFraction.numerator * firstFraction.denominator);
             BigInteger denominator = firstFraction.denominator * secondFraction.denominator;
             return new MyFraction(numerator, denominator);
+        }
+
+        public static MyFraction operator -(MyFraction firstFraction, MyFraction secondFraction)
+        {
+            BigInteger numerator = (firstFraction.numerator * secondFraction.denominator) - (secondFraction.numerator * firstFraction.denominator);
+            BigInteger denominator = (firstFraction.denominator * secondFraction.denominator);
+            return new MyFraction(numerator, denominator);
+        }
+
+        public static MyFraction operator /(MyFraction firstFraction, MyFraction secondFraction)
+        {
+
+            return new MyFraction(firstFraction.numerator * secondFraction.denominator, firstFraction.denominator * secondFraction.numerator);
         }
 
         public override string ToString()
